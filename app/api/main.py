8a1ab50375d04b64728612cbe4063171d1626b02
sa16 <28266026+sa16@users.noreply.github.com>
2026-02-23 20:13:25 -0500

    chore: update corsmiddleware

--- app/api/main.py
+++ app/api/main.py
@@ -37,7 +37,7 @@ app= FastAPI(
     )
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins=["http://localhost:5173", "http://localhost"],
+    allow_origins=["http://localhost:5173", "http://localhost", "https://ball-compliance-vercel.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
